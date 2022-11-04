99689f1438755a1c577800886f81908c5e2a6e83
Mandyiee <chiomzibae101@gmail.com>
2022-11-04 15:20:27 +0100

    mandy completed the django part

--- Huloex/urls.py
+++ Huloex/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 import userdashboard, visitors
 
 urlpatterns = [
@@ -25,3 +26,4 @@ urlpatterns = [
     path("", include('visitors.urls')),
     
 ]
+urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
